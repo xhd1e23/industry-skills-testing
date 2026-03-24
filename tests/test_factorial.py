029c191c5700fc455367e15848e77999c4f605b9
xhd1e23 <xhd1e23@soton.ac.uk>
2026-03-24 10:35:47 +0000

    Update test_factorial.py

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -2,5 +2,25 @@ import pytest
 from mymath.factorial import factorial
 
 
+def test_1():
+    assert factorial(3) == 1
+
+def test_2():
+    assert factorial(3) == 2
+
 def test_3():
     assert factorial(3) == 6
+
+def test_5():
+    assert factorial(5) == 120
+
+def test_10():
+    assert factorial(10) == 3628800
+
+def test_float():
+    with pytest.raises(ValueError):
+      factorial(1.50)
+
+def test_negative():
+    with pytest.raises(ValueError):
+      factorial(-1)
