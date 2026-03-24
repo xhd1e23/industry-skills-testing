d89ee86c7254397d46424995309e6104187107cb
xhd1e23 <xhd1e23@soton.ac.uk>
2026-03-24 10:33:30 +0000

    Update factorial.py

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -1,3 +1,9 @@
+def isInt(n):
+    """
+    Checks if the given number is in fact an integer
+    """
+	return ((n % 1) == 0)
+
 def factorial(n):
     """
     Calculate the factorial of a given number.
@@ -5,6 +11,11 @@ def factorial(n):
     :param int n: The factorial to calculate
     :return: The resultant factorial
     """
+    if isInt(n):
+         raise ValueError('Only use integers.')
+    elif n < 0:
+        raise ValueError('Only use non-negative integers.')
+
     factorial = 1
     for i in range(1, n + 1):
         factorial = factorial * i
